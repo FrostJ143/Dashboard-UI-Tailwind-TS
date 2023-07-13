03c50f344edcf18130f7573d2e2a961d3387b574
FrostJ143 <70150272+FrostJ143@users.noreply.github.com>
2023-07-13 23:15:40 +0700

    add navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,25 +1,30 @@
 const Navbar = () => {
     return (
-        <div className="flex justify-between items-center">
+        <div className="flex justify-between items-center px-5 py-2">
             <div className="flex-center">
                 <img src="../../public/conversionicon.svg" alt="" />
                 <span>Sangadmin</span>
             </div>
-            <div className="flex-center">
+            <div className="flex-center gap-x-20">
                 <div className="flex-center">
-                    <img src="../../public/search.svg" alt="" />
-                    <img src="../../public/app.svg" alt="" />
-                    <img src="../../public/expand.svg" alt="" />
-                    <img src="../../public/notifications.svg" alt="" className=""/>
+                    <img className="cursor-pointer " src="../../public/search.svg" alt="" />
+                    <img className="cursor-pointer ml-5" src="../../public/app.svg" alt="" />
+                    <img className="cursor-pointer ml-5" src="../../public/expand.svg" alt="" />
+                    <div className="cursor-pointer relative">
+                        <img className="ml-5" src="../../public/notifications.svg" alt="" />
+                        <span className="h-3.5 w-3.5 bg-red-600 rounded-full absolute flex-center -top-1 -right-2 text-xs">1</span>
+                    </div>
                 </div>
-                <div className="flex-center">
-                    <img src="../../public/noavatar.png" alt="admin avatar" className="w-5 h-5 rounded-full"/>
-                    <span>Sang</span>
+                <div className="flex-center cursor-pointer">
+                    <img src="../../public/noavatar.png" alt="admin avatar" className="w-6 h-6 rounded-full object-cover" />
+                    <span className="ml-3">Sang</span>
                 </div>
-                <div></div>
+                <div className="cursor-pointer">
+                    <img src="../../public/settings.svg" alt="" />
+                </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default Navbar;
